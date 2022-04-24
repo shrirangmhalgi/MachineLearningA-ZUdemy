0e27c30c27540023a818a788d0048a85df8ff90a
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2022-04-24 11:22:00 -0400

    explored the intution behind polynomial linear regression

---
+++ python codes/2. regression/3. polynomial linear regression/polynomial_linear_regression.py
@@ -0,0 +1,7 @@
+# y = b0 + b1x1 + b2x1^2 + .... + bnx1^n
+# limited use cases are for polynomial linear regression
+# eg. disease spread
+# even after there are different powers for x, why is it called polynomial 'linear' regression? 
+# when we are talking about polynomial linear regression, we are not talking about x but we are talking about the constant b0, b1, etc
+# y is a function of x can this function be expressed as a linear combination of the coefficients ultimately, they are the unknowns
+# our goal is to find the values of the coefficients and use those coefficients to plug in x and ultimately find the value of y
