7a8be8d8e31c4ff3539f2732b383b25f8db9fa21
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2022-04-27 01:52:53 -0400

    explored the intuition behind decision tree regression

---
+++ python codes/2. regression/5. decision tree regression/decision_tree_regression.py
@@ -0,0 +1,11 @@
+# CART : Classification and Regression Trees
+# there are 2 types of decision trees : classification trees and regression trees
+# regression trees are a bit more complex than classification trees
+# consider a scatter plot which represents our data there are 2 independent variables x1 and x2 and we want to predict the 3rd varibale which is a dependent variable y
+# the algorithm will be splitted into segments. just go to on splitting your data.
+# how the splits are conducted is decided by algorithm and it actually involves information entrophy
+# by each splitting are we deriving new information from the data? if yes then split if no then stop
+# each split is called as a leaf
+# it just creates a binary tree of conditions
+# the leaf contains the dependent variable 
+# the average of the terminal leaves is taken and then this is the value which is assigned to that leaf 
